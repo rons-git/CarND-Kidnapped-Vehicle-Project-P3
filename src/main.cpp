89e2d42a2e7197b9f89d2f769969c88393961b46
rons-git <ron@cancunron.com>
2018-11-26 20:49:03 -0500

    push from windows

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,8 @@ int main()
 	  return -1;
   }
 
+  cout << "Map is loaded" << endl;
+
   // Create particle filter
   ParticleFilter pf;
 
@@ -50,6 +52,8 @@ int main()
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
+
+    cout << "Data: " << data << endl;
 
     if (length && length > 2 && data[0] == '4' && data[1] == '2')
     {
